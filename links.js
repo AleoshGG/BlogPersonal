3bcda96795266f924b6dda8f6f8bba6e1d55ec17
Alexis Guzman <spartanthelexvro@gmail.com>
2023-11-15 16:00:22 -0600

    Maquetado Terminado

---
+++ links.js
@@ -0,0 +1,18 @@
+function face(url) {
+    window.location.href = url;
+}
+function insta(url) {
+    window.location.href = url;
+}
+function githup(url) {
+    window.location.href = url;
+}
+function link(url) {
+    window.location.href = url;
+}
+function x(url) {
+    window.location.href = url;
+}
+
+
+
