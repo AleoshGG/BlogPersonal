2632ac9d98d11a32950ccdba6e4c6d2b47fcb06f
Alexis Guzman <spartanthelexvro@gmail.com>
2023-11-14 19:21:00 -0600

    Registro iniciado

--- conextion.py
+++ conextion.py
@@ -1,11 +1,50 @@
-import mysql.connector
+from flask import Flask, request
 
-conexion = mysql.connector.connect(
-    host = 'localhost',
-    port = '3306',
-    user = 'root',
-    password = '117092',
-    database = 'holamundo'
-)
+app = Flask(__name__)
 
-print(conexion)
+# Configuración de la conexión a la base de datos
+# Reemplaza con tus propias credenciales y detalles de la base de datos
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '117092',
+    'database': 'holamundo',
+    'port': '3306'
+}
+
+# Ruta para insertar datos
+@app.route('/insertar_datos', methods=['GET'])
+def insertar_datos():
+    name = request.args.get('name')
+    email = request.args.get('email')
+
+    # Conexión y consulta SQL para la inserción
+    import mysql.connector
+
+    try:
+        # Establecer la conexión
+        conexion = mysql.connector.connect(**db_config)
+
+        # Crear un cursor
+        cursor = conexion.cursor()
+
+        # Ejecutar la inserción
+        insert_query = "INSERT INTO holamundo (name, email) VALUES (%s, %s)"
+        cursor.execute(insert_query, (name, email))
+
+        # Confirmar la transacción
+        conexion.commit()
+
+        return 'Datos insertados correctamente'
+
+    except Exception as e:
+        return f'Error al insertar datos: {str(e)}'
+
+    finally:
+        # Cerrar la conexión y el cursor
+        cursor.close()
+        conexion.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
